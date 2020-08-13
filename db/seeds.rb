c5867ef8eb867a997e33723c98628e0ccbc79690
Doerthe Kosicki <kiki@codestun.com>
2020-08-13 15:02:57 +0200

    plant seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,33 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Cleaning database..."
-Cocktails.destroy_all
 
-puts "Creating cocktails ..."
-mojito = { name: "Mojito" }
-margarita =  { name: "Margarita"}
+# Cocktails.destroy_all
+# Dose.destroy_all
+# Ingredient.destroy_all
 
-[ mojito, margarita ].each do |attributes|
-  cocktail = Cocktail.create!(attributes)
-  puts "Created #{cockktail.name}"
+# puts "Creating cocktails ..."
+# mojito = { name: "Mojito" }
+# margarita =  { name: "Margarita"}
+
+# [ mojito, margarita ].each do |attributes|
+#   cocktail = Cocktail.create!(attributes)
+#   puts "Created #{cockktail.name}"
+# end
+
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+result = JSON.parse(open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+drinks = result['drinks']
+
+drinks.each do |item|
+  item.each do |a, b|
+    Ingredient.create(name: b)
+  end
 end
-puts "Finished!"
+puts 'Finished!'
