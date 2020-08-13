57dfce710af364954459527001d6063faf502814
Doerthe Kosicki <kiki@codestun.com>
2020-08-13 16:25:41 +0200

    WIP

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,24 @@
 class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show, :edit, :update, :destroy]
+  
   def index
     @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,9 +1,19 @@
 class DosesController < ApplicationController
-  # def create
-  #   @cocktail = Cocktail.find([:cocktails_id])
-  # end
-
   def index
     @doses = Dose.all
+  end
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find([:cocktails_id])
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
   end
 end
